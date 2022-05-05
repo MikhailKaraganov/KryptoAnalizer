bb1c1e88f68cd5da5ffa65ffff5856e8b49839d2
MikWhite <mikwhite88@gmail.com>
2022-05-05 22:58:48 +0300

    Final too.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module mkara.krypto.kryptoanalizer {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires javafx.graphics;
 
     opens mkara.krypto.kryptoanalizer to javafx.fxml;
     exports mkara.krypto.kryptoanalizer;
